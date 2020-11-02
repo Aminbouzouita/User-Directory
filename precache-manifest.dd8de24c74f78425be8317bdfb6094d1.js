ea2af777ce4f11bf812b71eaccced9bb0fa402b2
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-02 14:52:44 -0800

    Updates

---
+++ precache-manifest.dd8de24c74f78425be8317bdfb6094d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5995d8d147480ab51823c72f32ba5c",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "06e14af2bf4d4d65b113",
+    "url": "/User-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "27115e40fa4d20d4eb96",
+    "url": "/User-Directory/static/css/main.18fc6c18.chunk.css"
+  },
+  {
+    "revision": "06e14af2bf4d4d65b113",
+    "url": "/User-Directory/static/js/2.0e072ab3.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/User-Directory/static/js/2.0e072ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27115e40fa4d20d4eb96",
+    "url": "/User-Directory/static/js/main.1a53bc9a.chunk.js"
+  },
+  {
+    "revision": "976d1ad4fb8038d1b6f5",
+    "url": "/User-Directory/static/js/runtime-main.8b6464b0.js"
+  }
+]);
